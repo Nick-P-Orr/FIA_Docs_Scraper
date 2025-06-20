6a7ee6545a37807d07751a1dfb6f4e0dbb4360d3
Nick Orr <nick.p.orr@gmail.com>
2025-06-20 11:57:45 -0400

    no idea what this change was for

--- scraper3.py
+++ scraper3.py
@@ -16,6 +16,10 @@ def get_fia_docs():
             button = driver.find_element_by_id('event-title')
             button.click()
             element = WebDriverWait(driver, 10).until(
+                EC.invisibility_of_element_located((By.ID, "deviceShowAllLink"))
+            )
+    # store it to string variable
+            page_source = driver.page_source
     # wait for the page to load
         
         0=0
